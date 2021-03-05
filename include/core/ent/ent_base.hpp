13e8ba92354ff9ad98e32d3d47580183a502e0df
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-05 01:20:47 -0600

    doc(ent_base): Update Documentation of ent_base

--- include/core/ent/ent_base.hpp
+++ include/core/ent/ent_base.hpp
@@ -1,8 +1,7 @@
 /**
  * @file entity_base.hpp
  * @author Oswaldo Rafael Zamora Ramirez (rafa.zamo.rals@comunidad.unam.mx)
- * @brief Clase de la que hereda cualquier entity del Motor
- * @version 0.1
+ * @version 0.0.1
  * @date 2020-08-03
  *
  * @copyright Copyright (c) Moon 2020 Oswaldo Rafael Zamora Ramírez
@@ -10,63 +9,61 @@
  */
 #pragma once
 
-// Alias de tipos
+/**
+ * \include alias.hpp
+ */
 #include <core/alias.hpp>
 
-#include <core/cmp/cmp_base.hpp>
-
-// Component_Base
+/**
+ * \include cmp/cmp_base.hpp
+ */
 #include <core/cmp/cmp_base.hpp>
 
 /**
- * @brief Namespace del core del Motor
- *
+ * @brief Namespace of the Core the Moon
+ * \namespace Moon::Core
  */
 namespace Moon::Core {
-    // Redefinicion de un vector de Tipos y Components
+    /** Redefinition of a vector of Types and Components*/
     using MapCmps_t =
       std::unordered_map<Moon::Alias::ComponentType, ComponentBase_t*>;
 
     /**
-     * @brief Clase de la que hereda cualquier entity del Motor
+     * @brief Class from which any entity of the Engine inherits
      * \image html assets/stability/stability_2.png
      */
     struct EntityBase_t
     {
 
-        /**Identificador del siguiente tipo de Entity*/
+        /**Identifier of the following type of Entity_t*/
         inline static Moon::Alias::EntityType nextType = 0;
 
-        /**Mapa de Tipos y Components*/
+        /** Map of Types and Components*/
         MapCmps_t components;
 
       public:
-        /**
-         * @brief Construct a new EntityBase_t object
-         *
-         */
+        /** Construct Default*/
         EntityBase_t() = default;
 
-        /**
-         * @brief Destroy the EntityBase_t object
-         *
-         */
+        /** Destroy the EntityBase_t object is virtual*/
         virtual ~EntityBase_t() {}
 
         /**
-         * @brief Añade un Component a this
+         * @brief Add a Component_t
          *
-         * @tparam CMP_t
-         * @param cmp
+         * @tparam CMP_t Type of the Component_t
+         * @pre CMP_t is base of Moon::Core::ComponentBase_t
+         * @param cmp pointer of Component_t
          */
         template<class CMP_t>
         requires std::is_base_of<Moon::Core::ComponentBase_t, CMP_t>::value void
         addComponent(CMP_t* cmp);
 
         /**
-         * @brief Get the Component object
+         * @brief Get the Component
          *
          * @tparam CMP_t
+         * @pre CMP_t is base of Moon::Core::ComponentBase_t
          * @return CMP_t*
          */
         template<class CMP_t>
