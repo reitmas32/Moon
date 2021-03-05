db4a66607c7ee5268a1dc30b692e16264e7ffffc
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-05 01:20:17 -0600

    doc(cmp): Update Documentation of cmp

--- include/core/cmp/cmp.hpp
+++ include/core/cmp/cmp.hpp
@@ -1,61 +1,76 @@
 /**
- * @file component.hpp
+ * @file cmp.hpp
  * @author Oswaldo Rafael Zamora Ramírez (rafa.zamo.rals@comunidad.unam.mx)
- * @brief Clase de la que heredan todos los components del Motor
- * @version 0.1
+ * @version 0.0.1
  * @date 2020-08-03
- *  
  * @copyright Copyright (c) Moon 2020 Oswaldo Rafael Zamora Ramírez
  *
  */
 #pragma once
 
 /**
- * \headerfile alias.hpp
+ * \include alias.hpp
  */
 #include <core/alias.hpp>
 
 /**
- * \headerfile cmp/cmp_base.hpp
+ * \include cmp/cmp_base.hpp
  */
 #include <core/cmp/cmp_base.hpp>
 
 /**
- * @brief Namespace del core del Motor
+ * @brief Namespace of the Core the Moon
  * \namespace Moon::Core
  */
 namespace Moon::Core {
-
     /**
-     * @brief Clase de la que heredan todos los Components del Motor
+     * @subsubsection Example
+     * @code{.cpp}
+     * struct Sprite_t : Moon::Core::Component_t<Sprite_t>{
+     * private:
+     *      std::vector<Image> sprite;
+     * public:
+     *      Sprite_t(){}
+     * 
+     *      Sprite_t(Moon::Alias::EntityId eid) : Component_t<Sprite_t>(eid){
+     *          //TODO:All Constructor
+     *      }
+     * 
+     *      ~Sprite(){
+     *          //TODO:All Destructor
+     *      }
+     * };
+     * @endcode
+     */
+    /**
+     * @brief Class from which all Engine Components inherit
+     * @subsubsection Stability
      * \image html assets/stability/stability_2.png
-     * @tparam Type
+     * @tparam Type Is a new Component Following the CRTP
      */
     template<class Type>
     struct Component_t : public ComponentBase_t
     {
-        /**
-         * @brief Construct a new Component_t object
-         *
-         */
+        /**Overloaded constructor Default*/
         Component_t();
 
         /**
-         * @brief Construct a new Component_t object
+         * Overloaded constructor with Moon::Alias::EntityId
          *
-         * @param eid Id de la ENtity a la que pertenece el Component
+         * @param eid Id of the Entity to which the Component belongs
          */
         Component_t(Moon::Alias::EntityId eid);
 
-        /**
-         * @brief Destroy the Component_t object
-         *
-         */
+        /** Destroy the Component_t object is virtual*/
         virtual ~Component_t() = 0;
 
         /**
          * @brief Get the Component Type object
-         *
+         * 
+         * @pre None
+         * 
+         * @post The return is unique for each Component_t
+         * 
          * @return Moon::Alias::ComponentType
          */
         static Moon::Alias::ComponentType getComponentType() noexcept;
