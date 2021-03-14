3883f83171280def81e99ea0d7917434ea0836a8
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-14 10:21:44 -0600

    feat(Moon_Logs): Middelware para optiizacion de Moon en modo Release

---
+++ include/tools/moon_log.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <spdlog/cfg/env.h>
+
+namespace Moon::Tools
+{
+    void Moon_Log(std::function<void()> log)
+    {
+#if not defined(RELEASE)
+        log();
+#endif
+    }
+
+} // namespace Moon::Tools
