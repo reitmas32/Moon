b46341062dd47c7a5085339eee61affe63420213
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-15 08:50:44 -0600

    feat(System): Se añadio el SystemVoid al core

--- include/core/sys/sys.hpp
+++ include/core/sys/sys.hpp
@@ -22,50 +22,67 @@
  * @brief Namespace del core del Motor
  *
  */
-namespace Moon::Core {
-    /**
+namespace Moon::Core
+{
+  /**
      * @brief Clase de la que heredan todos los Systems del Motor
      * \image html assets/stability/stability_2.png
      * @tparam Type
      */
-    template<Moon::Concepts::Ctx_t... Type>
-    struct System_t : public SystemBase_t
-    {
+  template <Moon::Concepts::Ctx_t... Type>
+  struct System_t : public SystemBase_t
+  {
 
-      public:
-        /**
+  public:
+    /**
          * @brief Contructor de System_t object
          *
          */
-        System_t();
+    System_t();
 
-        /**
+    /**
          * @brief Destructor de System_t object
          *
          */
-        virtual ~System_t() = 0;
+    virtual ~System_t() = 0;
 
-        /**
+    /**
          * @brief Get the System Type object
          *
          * @return Moon::Alias::SystemType
          */
-        static Moon::Alias::SystemType getSystemType() noexcept;
+    static Moon::Alias::SystemType getSystemType() noexcept;
 
-        /**
+    /**
          * @brief Metodo que actualiza al System
          *
          * @param gameContext
          */
-        virtual void update(Type*... gameContext) = 0;
+    virtual void update(Type *...gameContext) = 0;
 
-        /**
+    /**
          * @brief Metodo que indica si el system sigue alive
          *
          * @return true
          * @return false
          */
-        virtual bool alive() = 0;
-    };
+    virtual bool alive() = 0;
+  };
+
+  template <Moon::Concepts::Ctx_t... Type>
+  struct SystemVoid_t : public SystemBase_t
+  {
+
+  public:
+    SystemVoid_t();
+
+    virtual ~SystemVoid_t() = 0;
+
+    static Moon::Alias::SystemType getSystemType() noexcept;
+
+    virtual void update() = 0;
+
+    virtual bool alive() = 0;
+  };
 
 } // namespace Moon::Core
--- template/core/sys/sys.tpp
+++ template/core/sys/sys.tpp
@@ -2,19 +2,43 @@
 
 #include <core/sys/sys.hpp>
 
-namespace Moon::Core {
-    template<Moon::Concepts::Ctx_t... Type>
+namespace Moon::Core
+{
+    template <Moon::Concepts::Ctx_t... Type>
     System_t<Type...>::System_t()
-    {}
+    {
+    }
 
-    template<Moon::Concepts::Ctx_t... Type>
+    template <Moon::Concepts::Ctx_t... Type>
     System_t<Type...>::~System_t()
     {
         /*Nada*/
     }
 
-    template<Moon::Concepts::Ctx_t... Type>
+    template <Moon::Concepts::Ctx_t... Type>
     Moon::Alias::SystemType System_t<Type...>::getSystemType() noexcept
+    {
+        static Moon::Alias::SystemType type = ++nextType;
+        return type;
+    }
+
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+    //-----------------------------------------------------
+
+    template <Moon::Concepts::Ctx_t... Type>
+    SystemVoid_t<Type...>::SystemVoid_t()
+    {
+    }
+
+    template <Moon::Concepts::Ctx_t... Type>
+    SystemVoid_t<Type...>::~SystemVoid_t()
+    {
+        /*Nada*/
+    }
+
+    template <Moon::Concepts::Ctx_t... Type>
+    Moon::Alias::SystemType SystemVoid_t<Type...>::getSystemType() noexcept
     {
         static Moon::Alias::SystemType type = ++nextType;
         return type;
