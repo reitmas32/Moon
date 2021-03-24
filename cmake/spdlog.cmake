d6384edc6626452a2ae339f2a9b6c6469a81f9c7
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-24 11:52:32 -0600

    feat(dependencies): Add spdlog static

--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -1,5 +1,5 @@
-add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/spdlog)
+#add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/spdlog)
 
 set(SPDLOG_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/vendor/spdlog/include)
 
-set(SPDLOG_LIBRARIES ${GLFW_LIBRARIES} spdlog)
+set(SPDLOG_LIBRARIES ${GLFW_LIBRARIES} ${CMAKE_SOURCE_DIR}/vendor/spdlog/build/libspdlog.a)
