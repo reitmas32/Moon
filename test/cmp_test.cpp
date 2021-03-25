d67420adb23aa3ca1205e6145b678228f3d77a06
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-24 20:51:17 -0600

    feat(test): Add Test for new gtx

--- test/cmp_test.cpp
+++ test/cmp_test.cpp
@@ -2,14 +2,82 @@
 
 #include <gtest/gtest.h>
 
+#include <core/ent/ent.tpp>
+
+#include <core/ent/ent_vect.tpp>
+
+#include <core/ent/ent_storage.tpp>
+
 #include <core/cmp/cmp.tpp>
+
+#include <core/cmp/cmp_storage.tpp>
+
+#include <core/ent/ent.tpp>
+
+#include <core/ent/ent_base.tpp>
+
+#include <core/gtx/gtx.tpp>
 
 TEST(Test, example)
 {
     struct CMP_TEST_t : Moon::Core::Component_t<CMP_TEST_t>
     {
-        /* data */
+        int data;
+        CMP_TEST_t(Moon::Alias::EntityId eid, int data) : Moon::Core::Component_t<CMP_TEST_t>(eid), data{data} {}
     };
 
+    struct ENT_TEST_1_t : Moon::Core::Entity_t<ENT_TEST_1_t>
+    {
+        ENT_TEST_1_t(Moon::Alias::EntityId eid) : Moon::Core::Entity_t<ENT_TEST_1_t>(eid) {}
+    };
+
+    struct ENT_TEST_2_t : Moon::Core::Entity_t<ENT_TEST_2_t>
+    {
+        ENT_TEST_2_t(Moon::Alias::EntityId eid) : Moon::Core::Entity_t<ENT_TEST_2_t>(eid) {}
+    };
+
+    struct GTX_TEST_1_t : Moon::Core::GameContext_t<ENT_TEST_1_t>
+    {
+    };
+
+    auto gtx = GTX_TEST_1_t();
+
+    auto &e1 = gtx.addEntity<ENT_TEST_1_t>();
+
+    auto &e2 = gtx.addEntity<ENT_TEST_2_t>();
+
+    auto *e_1 = gtx.getEntityById<ENT_TEST_2_t>(2);
+
+    auto &c1 = gtx.addComponentById<ENT_TEST_1_t, CMP_TEST_t>(e1.eid, 5);
+
+    gtx.destroyEntityById<ENT_TEST_1_t>(e1.eid);
+
+    EXPECT_EQ(1, gtx.getComponents<CMP_TEST_t>().size());
+    EXPECT_EQ(1, gtx.getEntities<ENT_TEST_1_t>().size());
+    EXPECT_EQ(1, gtx.getEntities<ENT_TEST_2_t>().size());
+
     EXPECT_EQ(1, CMP_TEST_t::getComponentType());
+    EXPECT_EQ(1, ENT_TEST_1_t::getEntityType());
+    EXPECT_EQ(2, ENT_TEST_2_t::getEntityType());
+    EXPECT_EQ(c1.getComponentType(), CMP_TEST_t::getComponentType());
+}
+
+
+TEST(Test, example_1)
+{
+    struct ENT_TEST_2_t : Moon::Core::Entity_t<ENT_TEST_2_t>
+    {
+        ENT_TEST_2_t(Moon::Alias::EntityId eid) : Moon::Core::Entity_t<ENT_TEST_2_t>(eid) {}
+    };
+
+    struct ENT_TEST_1_t : Moon::Core::Entity_t<ENT_TEST_1_t>
+    {
+        ENT_TEST_1_t(Moon::Alias::EntityId eid) : Moon::Core::Entity_t<ENT_TEST_1_t>(eid) {}
+    };
+
+    auto storage = Moon::Core::EntityStorage_t();
+
+    auto e1 = storage.createEntity<ENT_TEST_1_t>(1);
+    auto& vect_1 = storage.getEntities<ENT_TEST_1_t>();
+    EXPECT_EQ(1, vect_1.size());
 }
