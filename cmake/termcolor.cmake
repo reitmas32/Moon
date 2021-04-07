c9eda7d867b55da0ccfb5db45e2b87d59a56978e
reitmas32 <rafa.zamora.rals@gmail.com>
2021-04-06 21:39:24 -0500

    feat(cmake): Update TERMCOLOR_INCLUDE_DIR

--- cmake/termcolor.cmake
+++ cmake/termcolor.cmake
@@ -1,3 +1,3 @@
-set(TERMCOLOR_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/vendor/termcolor/include)
+set(TERMCOLOR_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/vendor/termcolor)
 
 set(TERMCOLOR_LIBRARIES ${CMAKE_SOURCE_DIR}/vendor/termcolor/build/libtermcolor.a)
