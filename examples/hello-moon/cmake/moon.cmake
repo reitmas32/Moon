35223f62e76e11ca9aff5bbd2bc6d9e8ab042f14
reitmas32 <rafa.zamora.rals@gmail.com>
2021-05-05 21:27:46 -0500

    feat(examples): Create Config for windows

--- examples/hello-moon/cmake/moon.cmake
+++ examples/hello-moon/cmake/moon.cmake
@@ -5,14 +5,22 @@
 #########################
 if(${WIN32})
     
-    add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/Moon)
-    
-    set(MOON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/vendor/Moon/include ${CMAKE_SOURCE_DIR}/vendor/Moon/template ${CMAKE_SOURCE_DIR}/vendor/Moon/vendor/termcolor)
-    set(MOON_LIBRARIES ${MOON_LIBRARIES} moon)
-    
+    #   Dir of MoonStudio
+    set(MOON_STUDIO_DIR $ENV{MOON_STUDIO_DIR})
+
+    set(MOON_INCLUDE_DIR 
+        ${MOON_STUDIO_DIR} 
+        ${MOON_STUDIO_DIR}/Moon/template 
+        ${MOON_STUDIO_DIR}/Moon/include 
+        ${MOON_STUDIO_DIR}/Moon/vendor/termcolor)
+
+    message(${MOON_INCLUDE_DIR})
     option(LOGS "ON")
     if(${LOGS} STREQUAL "OFF")
-    add_compile_definitions(RELEASE)
+        #add_compile_definitions(RELEASE)
+        set(MOON_LIBRARIES ${MOON_STUDIO_DIR}/Moon/build/moon.lib)
+    else()
+        set(MOON_LIBRARIES ${MOON_STUDIO_DIR}/Moon/build/moon.lib)
     endif()
 
 elseif(${UNIX})
