c9667b1debb80d415480deb5b7a3a1c2ea5d32de
reitmas32 <rafa.zamora.rals@gmail.com>
2021-05-07 08:33:32 -0500

    feat(example): Add Code of the example

--- examples/hello-moon/src/main.cpp
+++ examples/hello-moon/src/main.cpp
@@ -7,6 +7,10 @@
 
 //Component
 #include <Moon/template/core/cmp/cmp.tpp>
+#include <Moon/template/core/cmp/cmp_storage.tpp>
+#include <Moon/template/core/ent/ent_base.tpp>
+#include <Moon/template/core/ent/ent.tpp>
+#include <Moon/template/core/gtx/gtx.tpp>
 
 //MyComponent
 struct MyCmp_t : Moon::Core::Component_t<MyCmp_t>
@@ -25,9 +29,40 @@ struct MyCmp_t : Moon::Core::Component_t<MyCmp_t>
     ~MyCmp_t() = default;   
 };
 
+struct MyCmp1_t : Moon::Core::Component_t<MyCmp1_t>
+{
+    //data of Component
+    int data;
+
+    //Constructors
+    MyCmp1_t(Moon::Alias::EntityId eid) : Moon::Core::Component_t<MyCmp1_t>(eid) {}
+    MyCmp1_t(
+        Moon::Alias::EntityId eid,
+        int data) : Moon::Core::Component_t<MyCmp1_t>(eid), data{data}{}
+    MyCmp1_t() = default;
+
+    //Destructors
+    ~MyCmp1_t() = default;   
+};
+
+struct Player : Moon::Core::Entity_t<Player>
+{
+    Player(Moon::Alias::EntityId eid) : Moon::Core::Entity_t<Player>(eid) {}
+    ~Player(){}
+};
+
+struct Gtx_t : Moon::Core::GameContext_t<Gtx_t>{
+
+};
+
 int main(int argc, char const *argv[])
 {
-    MyCmp_t cmp = MyCmp_t(0, 45); // eid = 0, data = 45
-    std::cout << "MyCmp_t eid: "<<cmp.eid<<", data: " << cmp.data << std::endl;
+    auto gtx = Gtx_t();
+    auto & e1 = gtx.addEntity<Player>();
+    auto & e2 = gtx.addEntity<Player>();
+    auto &c1 = gtx.addComponentById<Player, MyCmp_t>(e1.eid, 5);
+    auto &c3 = gtx.addComponentById<Player, MyCmp1_t>(e1.eid, 1);
+    auto &c2 = gtx.addComponentById<Player, MyCmp_t>(e2.eid, 4);
+    gtx.destroyEntityById<Player>(e1.eid);
     return 0;
 }
